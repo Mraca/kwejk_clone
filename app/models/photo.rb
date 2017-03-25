2404e02fae4f53499be4dfd5d08233e6a0f94e29
Your Name <you@example.com>
2017-03-25 18:52:26 +0100

    add validates to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ActiveRecord::Base
+	validates :title, presence: true, length: { minimum: 5 }
+
 	has_attached_file :image, styles: { large: "400x600>", medium: "300x300>", thumb: "100x100#" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
