d0dee35792111dcb593c9f001da3eeadbab4d35f
Your Name <you@example.com>
2017-03-25 18:57:36 +0100

    add option delele photos with comments

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,5 +5,5 @@ class Photo < ActiveRecord::Base
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   	belongs_to :user
-  	has_many :comments
+  	has_many :comments, dependent: :destroy
 end
